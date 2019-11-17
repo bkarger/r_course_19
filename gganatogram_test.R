0c241caf835edc39c4cac6ca4da0ba8ef4055d98
Burton Karger <burton.karger@gmail.com>
2019-11-17 14:54:49 -0700

    Using mouse now instead of human for plot.

--- gganatogram_test.R
+++ gganatogram_test.R
@@ -1,11 +1,14 @@
 library(tidyverse)
-
-organ_plot <- data.frame(organ = c("heart", "leukocyte", "liver", "lung", "spleen"),
-                         colour = c("red", "green", "red", "blue", "green"),
-                         type = c("circulation", "immune", "circulation", "respiratory", "immune"),
-                         value = c(2, 5, 1, 8 , 2),
+library(gganatogram)
+organ_plot <- data.frame(organ = c("heart", "aorta", "lymph_node", "liver", "lung", "spleen"),
+                         colour = c("red", "red", "green", "brown", "blue", "purple"),
+                         type = c("circulation", "circulation", "immune", "digestion", "respiratory", "immune"),
+                         value = c(2, 5, 1, 8 , 2, 7),
                          stringsAsFactors = F)
-head(organ_plot)
-gganatogram(data = mmMale_key, outline = T, fillOutline = "#a6bddb", 
-            organism = 'mouse', sex = "male", fill = "colour")
+
+ga <- gganatogram(data = organ_plot, outline = T, fillOutline = "#a6bddb", 
+            organism = 'mouse', sex = "male", fill = "colour") +
+  ggtitle("Impact BAA (CSU, IDRI, OXFORD)",  subtitle = "Mouse ex.") +
+  theme_void()
+ga
 
