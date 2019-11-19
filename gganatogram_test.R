43ae1804e64e042bd436412e12fd6935c84d74e9
Burton Karger <burton.karger@gmail.com>
2019-11-18 19:08:29 -0700

    clean up graph.

--- gganatogram_test.R
+++ gganatogram_test.R
@@ -1,14 +1,31 @@
 library(tidyverse)
 library(gganatogram)
-organ_plot <- data.frame(organ = c("heart", "aorta", "lymph_node", "liver", "lung", "spleen"),
-                         colour = c("red", "red", "green", "brown", "blue", "purple"),
-                         type = c("circulation", "circulation", "immune", "digestion", "respiratory", "immune"),
+library(gridExtra)
+mouse_organ <- data.frame(organ = c("heart", "aorta", "lymph_node", "liver", "lung", "spleen"),
+                         colour = c("green", "green", "gold", "green", "gold", "green"),
+                         type = c("circulation", "circulation", "immune", 
+                                  "digestion", "respiratory", "immune"),
+                         value = c(2, 5, 1, 8 , 2, 7),
+                         stringsAsFactors = F)
+human_organ <- data.frame(organ = c("heart", "aorta", "lymph_node", "liver", "lung", "spleen"),
+                         colour = c("green", "green", "gold", "green", "gold", "green"),
+                         type = c("circulation", "circulation", "immune", 
+                                  "digestion", "respiratory", "immune"),
                          value = c(2, 5, 1, 8 , 2, 7),
                          stringsAsFactors = F)
 
-ga <- gganatogram(data = organ_plot, outline = T, fillOutline = "#a6bddb", 
+mouse_CSU <- gganatogram(data = mouse_organ, outline = T, fillOutline = "peachpuff2", 
             organism = 'mouse', sex = "male", fill = "colour") +
   ggtitle("Impact BAA (CSU, IDRI, OXFORD)",  subtitle = "Mouse ex.") +
-  theme_void()
-ga
+  theme_void()+
+  theme(panel.background = element_rect(fill = "black"))
 
+human_CSU <- gganatogram(data = human_organ, outline = T, fillOutline = "peachpuff2", 
+                         organism = 'human', sex = "male", fill = "colour") +
+  ggtitle("Impact BAA (CSU, IDRI, OXFORD)",  subtitle = "Human ex.") +
+  theme_void() +
+  theme(panel.background = element_rect(fill = "black"))
+
+
+combined_plots <- grid.arrange(human_CSU, mouse_CSU, nrow = 1) 
+  
